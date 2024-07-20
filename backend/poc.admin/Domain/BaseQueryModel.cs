b5baf8d80753f56907af42e280f8ccd8821f8533
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2024-07-20 19:46:37 -0300

    -

---
+++ backend/poc.admin/Domain/BaseQueryModel.cs
@@ -0,0 +1,8 @@
+﻿using poc.core.api.net8.Abstractions;
+
+namespace poc.admin.Domain;
+
+public abstract class BaseQueryModel : IQueryModel<Guid>
+{
+    public Guid Id { get; private init; } = Guid.NewGuid();
+}
