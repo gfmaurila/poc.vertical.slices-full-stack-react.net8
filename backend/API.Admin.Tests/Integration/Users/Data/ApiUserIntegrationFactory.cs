8495074ff88753fa375906d524dd455b5af11431
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2024-08-02 16:43:53 -0300

    -

--- backend/API.Admin.Tests/Integration/Users/Data/ApiUserIntegrationFactory.cs
+++ backend/API.Admin.Tests/Integration/Users/Data/ApiUserIntegrationFactory.cs
@@ -9,5 +9,17 @@ public class ApiUserIntegrationFactory<TProgram> : WebApplicationFactory<TProgra
     {
         Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Test");
         builder.UseEnvironment("Test");
+
+        //Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Docker");
+        //builder.UseEnvironment("Docker");
+
+        //Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+        //builder.UseEnvironment("Development");
+
+        //Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Production");
+        //builder.UseEnvironment("Production");
+
+        //Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Staging");
+        //builder.UseEnvironment("Staging");
     }
 }
